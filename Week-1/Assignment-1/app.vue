faf29b356e8d224689dbad2003db5f35801f9f64
timsu92 <33785401+timsu92@users.noreply.github.com>
2023-06-20 18:57:36 +0800

    feat(hw1): put hello world in page

--- Week-1/Assignment-1/app.vue
+++ Week-1/Assignment-1/app.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="w-screen h-screen bg-gray-200 dark:bg-gray-900 text-gray-900 dark:text-gray-200 flex justify-center items-center font-kablammo text-6xl">
+    Hello World!
   </div>
 </template>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Kablammo&display=swap');
+</style>
