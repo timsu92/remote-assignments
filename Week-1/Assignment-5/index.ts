c630c9ca6fa4d137c8e5ff34e5a86539b528f44a
timsu92 <33785401+timsu92@users.noreply.github.com>
2023-07-02 19:47:29 +0800

    feat(hw5): complete hw5

--- Week-1/Assignment-5/index.ts
+++ Week-1/Assignment-5/index.ts
@@ -7,6 +7,17 @@ app.get('/', function (req, res) {
 	res.send("Hello, My Server!\nReturned from localhost~");
 })
 
+app.get('/data', function (req, res) {
+	const query: {number?: string} = req.query;
+	if(Object.keys(query).length === 0) {
+		res.send("Lack of Parameter");
+	} else if (query.number === undefined || isNaN(+query.number) || !Number.isInteger(+query.number) || +query.number < 1) {
+		res.send('Wrong Parameter');
+	} else {
+		res.send(((1 + +query.number) * +query.number / 2).toString(10));
+	}
+})
+
 app.listen(port, () => {
 	console.log(`Example app listening on localhost:${port}`);
 })
